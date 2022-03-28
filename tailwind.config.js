4a2cc11b7a5d0a04806b0e00e346e1b5a6c6cc1e
yohan <hijr88@naver.com>
2022-03-28 17:19:35 +0900

    prettier 설정 파일 추가, tailwind 설정 방법 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+const plugin = require("tailwindcss/plugin");
 const jsDir = "./js/src";
 
 module.exports = {
   content: [jsDir + "/**/*.js", "./html/**/*.html"],
+  corePlugins: {
+    //초기 세팅 제거
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        //초기세팅에 추가할것
+        button: {
+          border: 0,
+          cursor: "pointer",
+          "font-family": "inherit",
+        },
+      });
+    }),
+  ],
   prefix: "",
 };
